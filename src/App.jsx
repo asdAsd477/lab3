8a6627ee95ae9f09670a658500a902e3da2a6579
asdAsd477 <nowikov.denn1s@yandex.ru>
2026-04-18 17:21:29 +0300

    vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Typography, Button } from 'antd'
+
+const { Title } = Typography
+
+export default function App() {
+	return (
+		<>
+			<Title>A smart title</Title>
+			<Button type="primary">button</Button>
+		</>
+	)
+}
