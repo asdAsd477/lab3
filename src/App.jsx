bce4881105a58285386f10c9ca95add8c6eb1292
asdAsd477 <nowikov.denn1s@yandex.ru>
2026-04-18 23:11:34 +0300

    add dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
-import { Typography, Button } from 'antd'
+import Header from '@/layout/Header.jsx'
+import { Layout, ConfigProvider, theme } from 'antd'
+import { Outlet } from 'react-router'
 
-const { Title } = Typography
+const { Content } = Layout
+
+const customTheme = {
+	algorithm: theme.darkAlgorithm,
+	token: {
+		fontSize: 18,
+		controlHeight: 40,
+
+		colorBgLayout: '#181818',
+		colorBgContainer: '#2a2a2a',
+
+		colorPrimary: '#52c41a',
+		colorLink: '#73d13d',
+	},
+	components: {
+		Layout: {
+			headerBg: '#101010',
+			headerHeight: 80,
+			headerPadding: '0 50px',
+		},
+		Typography: {
+			titleMarginBottom: 0,
+			titleMarginTop: 0,
+		},
+		List: {
+			colorSplit: '#333',
+		},
+
+		Form: {
+			itemMarginInlineEnd: 0,
+			itemMarginBottom: 0,
+		}
+	}
+}
 
 export default function App() {
 	return (
-		<>
-			<Title>A smart title</Title>
-			<Button type="primary">button</Button>
-		</>
+		<ConfigProvider theme={customTheme}>
+			<Layout style={{ minHeight: '100vh' }}>
+				<Header />
+				<Content>
+					<Outlet />
+				</Content>
+			</Layout>
+		</ConfigProvider>
 	)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 
-import App from './App.jsx'
+import './global.css'
+
+import App from '@/App.jsx'
+import HomePage from '@/pages/HomePage.jsx'
+import AboutPage from '@/pages/AboutPage.jsx'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{ path: '/', element: <HomePage /> },
+			{ path: '/about', element: <AboutPage /> },
+		]
+	}
+])
 
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+	<RouterProvider router={router} />
 )
