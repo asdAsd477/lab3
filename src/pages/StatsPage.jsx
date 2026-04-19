416cc7a7313029e4f64ef41f53c766b7cdd0e1a4
asdAsd477 <nowikov.denn1s@yandex.ru>
2026-04-19 23:52:31 +0300

    add Stats page

---
+++ src/pages/StatsPage.jsx
@@ -0,0 +1,28 @@
+import HabitChart from '@/components/HabitChart.jsx';
+import { useHabitStore } from '@/hooks/useHabitStore.js';
+import Container from '@/layout/Container.jsx'
+
+import { Flex, Typography } from 'antd'
+const { Title, Text } = Typography
+
+export default function StatsPage() {
+	const history = useHabitStore(s => s.history);
+
+	// 1. Сколько раз всего были нажаты чекбоксы
+	const totalSuccess = Object.values(history).flat().length
+
+	// 2. Самый продуктивный день
+	const dates = Object.keys(history)
+	const bestDate = dates.sort((a, b) => history[b].length - history[a].length)[0]
+
+	return (
+		<Container width={1000}>
+			<Title level={2}>📈 Статистика</Title>
+			<Flex vertical gap="small">
+				<p>🎯 Всего выполнено <Text strong>{totalSuccess}</Text> привычек!</p>
+				<p>✨ Самый продуктивный день был <Text strong>{bestDate}</Text></p>
+			</Flex>
+			<HabitChart />
+		</Container>
+	)
+}
